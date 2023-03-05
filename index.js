c850d70360f07e06c8f68a0df8f64b9c0be76fd8
Sweety singh <shalusingh0916@gmail.com>
2023-03-05 19:57:57 +0530

    add my other files

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 console.log("hello sweety lets start node js new course");
 console.log("all the best for your placement jurney");
 console.log("love you sweety")
-console.log("apne hosle ko buland kr tujhe koi nhi rok skta tere mahadev tere sath hai sirf mehanat krti ja teri kismat teri manjil dilayegi")
+console.log("waooooooo  i done my git commit")
